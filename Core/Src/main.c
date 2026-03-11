7e1e54c8adcd85d3f16e5e01f5cf6fe8daef1976
dB-XIX <berrydb19@gmail.com>
2026-03-10 23:30:17 -0500

    Implement STM32 button LED state machine with short and long press handling

---
+++ Core/Src/main.c
@@ -0,0 +1,456 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+typedef enum
+{
+    MODE_OFF = 0,
+    MODE_ON,
+    MODE_FLASH,
+    MODE_SOS
+} LedMode_t;
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+#define LONG_PRESS_TIME_MS   1000
+#define DEBOUNCE_TIME_MS     50
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+uint32_t currentTime = 0;
+uint32_t previousTime = 0;
+uint32_t buttonPressStart = 0;
+
+volatile LedMode_t ledMode = MODE_OFF;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+void quick_flash(void);
+void sos_pattern(void);
+uint8_t delay_abortable(uint32_t ms);
+uint8_t led_on_abortable(uint32_t ms);
+uint8_t led_off_abortable(uint32_t ms);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+// Interrupt callback function
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    if (GPIO_Pin == GPIO_PIN_8)
+    {
+        currentTime = HAL_GetTick();
+
+        if ((currentTime - previousTime) < DEBOUNCE_TIME_MS)
+        {
+            return;
+        }
+
+        previousTime = currentTime;
+
+        if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) == GPIO_PIN_SET)
+        {
+            // Button pressed
+            buttonPressStart = currentTime;
+        }
+        else
+        {
+            // Button released
+            uint32_t pressDuration = currentTime - buttonPressStart;
+
+            if (pressDuration >= LONG_PRESS_TIME_MS)
+            {
+                // Long press turns LED off from any mode
+                ledMode = MODE_OFF;
+            }
+            else
+            {
+                // Short press cycles only through the 3 active modes
+                switch (ledMode)
+                {
+                    case MODE_OFF:
+                        ledMode = MODE_ON;
+                        break;
+
+                    case MODE_ON:
+                        ledMode = MODE_FLASH;
+                        break;
+
+                    case MODE_FLASH:
+                        ledMode = MODE_SOS;
+                        break;
+
+                    case MODE_SOS:
+                        ledMode = MODE_ON;
+                        break;
+
+                    default:
+                        ledMode = MODE_OFF;
+                        break;
+                }
+            }
+        }
+    }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+      switch (ledMode)
+      {
+          case MODE_OFF:
+              HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+              break;
+
+          case MODE_ON:
+              HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+              break;
+
+          case MODE_FLASH:
+              quick_flash();
+              break;
+
+          case MODE_SOS:
+              sos_pattern();
+              break;
+
+          default:
+              ledMode = MODE_OFF;
+              break;
+      }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 180;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Activate the Over-Drive mode
+  */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+uint8_t delay_abortable(uint32_t ms)
+{
+    uint32_t startTime = HAL_GetTick();
+    LedMode_t startingMode = ledMode;
+
+    while ((HAL_GetTick() - startTime) < ms)
+    {
+        if (ledMode != startingMode)
+        {
+            return 1;
+        }
+
+        HAL_Delay(10);
+    }
+
+    return 0;
+}
+
+uint8_t led_on_abortable(uint32_t ms)
+{
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+    return delay_abortable(ms);
+}
+
+uint8_t led_off_abortable(uint32_t ms)
+{
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+    return delay_abortable(ms);
+}
+
+void quick_flash(void)
+{
+    if (led_off_abortable(1000)) return;
+    if (led_on_abortable(100)) return;
+}
+
+void sos_pattern(void)
+{
+    // S
+    if (led_on_abortable(200)) return;
+    if (led_off_abortable(200)) return;
+
+    if (led_on_abortable(200)) return;
+    if (led_off_abortable(200)) return;
+
+    if (led_on_abortable(200)) return;
+    if (led_off_abortable(600)) return;
+
+    // O
+    if (led_on_abortable(600)) return;
+    if (led_off_abortable(200)) return;
+
+    if (led_on_abortable(600)) return;
+    if (led_off_abortable(200)) return;
+
+    if (led_on_abortable(600)) return;
+    if (led_off_abortable(600)) return;
+
+    // S
+    if (led_on_abortable(200)) return;
+    if (led_off_abortable(200)) return;
+
+    if (led_on_abortable(200)) return;
+    if (led_off_abortable(200)) return;
+
+    if (led_on_abortable(200)) return;
+    if (led_off_abortable(1000)) return;
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ README.md
@@ -0,0 +1,136 @@
+# STM32F446 Button LED State Machine
+
+Embedded C project for the STM32 Nucleo-F446 built using STM32CubeIDE.  
+This project demonstrates interrupt-driven button input with short-press and long-press detection controlling multiple LED modes.
+
+---
+
+## Features
+
+- Short press cycles LED modes
+  - Solid ON
+  - Quick flash
+  - SOS pattern
+
+- Long press turns LED OFF from any mode
+
+- Uses EXTI interrupts instead of polling
+- Implements software debouncing
+- Detects button press and release using rising and falling edge interrupts
+- Uses interruptible timing so flashing and SOS patterns can stop immediately when the button is pressed
+
+---
+
+## Hardware
+
+- STM32 Nucleo-F446
+- Breadboard
+- Pushbutton
+- LED
+- Current-limiting resistor
+- Jumper wires
+
+---
+
+## Pin Configuration
+
+| Pin | Function |
+|-----|----------|
+| PA8 | Button input |
+| PB5 | LED output |
+
+---
+
+## GPIO Setup
+
+Button input:
+
+PA8 → GPIO_MODE_IT_RISING_FALLING  
+PA8 → GPIO_PULLDOWN
+
+LED output:
+
+PB5 → GPIO_MODE_OUTPUT_PP
+
+---
+
+## Behavior
+
+Short press cycles through LED modes:
+
+OFF → ON → FLASH → SOS → ON
+
+Long press from any mode turns the LED off:
+
+(any mode) → OFF
+
+---
+
+## LED Modes
+
+### Solid ON
+LED remains continuously on.
+
+### Quick Flash
+LED remains mostly off with a brief flash:
+
+OFF 1000 ms  
+ON 100 ms
+
+### SOS Pattern
+
+Standard Morse code SOS pattern:
+
+S = short short short  
+O = long long long  
+S = short short short
+
+---
+
+## Concepts Demonstrated
+
+This project demonstrates several important embedded firmware concepts:
+
+- Embedded C programming
+- STM32 HAL usage
+- GPIO configuration
+- External interrupt handling (EXTI)
+- Button debouncing
+- Short-press vs long-press detection
+- Finite state machines
+- Timing using HAL_GetTick()
+- Interrupt-safe shared variables (volatile)
+- Interruptible delay patterns
+
+---
+
+## Development Environment
+
+- STM32CubeIDE
+- STM32 HAL Drivers
+- ARM GCC Toolchain
+
+---
+
+## Project Structure
+
+Core/  
+Drivers/  
+main.c  
+STM32xxxx.ioc  
+README.md  
+.gitignore  
+
+The `.ioc` file allows the project to be reopened and modified using STM32CubeMX inside STM32CubeIDE.
+
+---
+
+## Purpose
+
+This project was created as a learning exercise to practice real embedded firmware patterns beyond simple LED blinking, including interrupt-driven input handling and state-machine based behavior control.
+
+---
+
+## Author
+
+Michael Berry
